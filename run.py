57789fe0be90421098a7c24190ea363b29e4d495
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-19 02:46:24 +0300

    Fix run.py

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import os
 
-root = '/main/mnt/calypso/24a_kir/MSU_VSR_Benchmark/input'
-
-_, dirs, _ = next(os.walk(root))
-
-for dir in dirs:
-    if 'noise' in dir:
-        os.system(f'python3 test_multiple_frames.py {root}/{dir} /main/mnt/calypso/25e_zim/VSR_noise/TMNet/{dir}')
+os.system("cd /model/models/modules/DCNv2")
+os.system("bash make.sh")
+os.system("cd ../../../")
+os.system("mkdir result")
+os.system("python3 test_multiple_frames.py /dataset /model/result")
+os.system("rm -r result/adobe")
--- run.sh
+++
@@ -1,6 +0,0 @@
-cd /model/models/modules/DCNv2
-bash make.sh
-cd ../../../
-mkdir result
-python3 test_multiple_frames.py /dataset /model/result
-rm -r result/adobe
