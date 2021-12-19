75a2bc158fdbbe108d477cdf13b463b783750808
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-19 13:00:22 +0300

    Update permissions

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ with open("run.sh", 'w') as f:
     f.write("python3 test_multiple_frames.py /dataset /model/result\n")
     f.write("rm -r result/adobe\n")
     f.write("chmod -R 0777 results\n")
+    f.write("chmod -R 0777 /model")
 
 os.system("chmod +x run.sh")
 os.system("./run.sh")
