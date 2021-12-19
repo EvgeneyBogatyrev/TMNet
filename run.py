7bf9354cfdea49be03823cf88c247713ae724922
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-19 13:04:14 +0300

    Last commit

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ with open("run.sh", 'w') as f:
     f.write("mkdir result\n")
     f.write("python3 test_multiple_frames.py /dataset /model/result\n")
     f.write("rm -r result/adobe\n")
-    f.write("chmod -R 0777 results\n")
     f.write("chmod -R 0777 /model")
 
 os.system("chmod +x run.sh")
