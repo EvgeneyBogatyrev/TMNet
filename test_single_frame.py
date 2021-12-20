b207fd7d2f45b77ef5704cc704625ca9c0efe845
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-20 18:51:52 +0300

    Enable GPU

--- test_single_frame.py
+++ test_single_frame.py
@@ -4,13 +4,13 @@ import sys
 test_args = {}
 
 # DEFAULT =============================================================
-test_args['cuda'] = '1'
+test_args['cuda'] = '0'
 test_args['code_name'] = 'TMNet'
 test_args['model_path'] = '/model/checkpoints/tmnet_single_frame.pth'
 test_args['result_folder'] = sys.argv[2]
 
 # DATASET =============================================================
-test_args['data_mode'] = 'vid4'
+test_args['data_mode'] = 'adobe'
 test_args['dataset_folder'] =  sys.argv[1]
 
 # test_args['data_mode'] = 'vimeo_fast'
