2861d3ffeec65d9af13803324e076bb05995b52a
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-25 18:50:17 +0300

    Make TMNet work with multiple videos

--- run.py
+++ run.py
@@ -1,15 +1,20 @@
 import os
+from pathlib import Path
 
-with open("/model/models/modules/DCNv2/mak.sh", 'w') as g:
+with open("/model/models/modules/DCNv2/make.sh", 'w') as g:
     g.write("python3 setup.py build develop\n")
 
 with open("run.sh", 'w') as f:
     f.write("cd /model/models/modules/DCNv2\n")
-    f.write("bash mak.sh\n")
+    f.write("bash make.sh\n")
     f.write("cd ../../../\n")
-    f.write("mkdir result\n")
-    f.write("python3 test_single_frame.py /dataset /model/result\n")
-    f.write("rm -r result/adobe\n")
+    #f.write("mkdir result\n")
+
+    videos = os.listdir("/dataset")
+    for video in videos:
+        Path(f"/model/result/{video}").mkdir(parents=True, exist_ok=True)
+        f.write(f"python3 test_single_frame.py /dataset/{video} /model/result/{video}\n")
+        f.write(f"rm -r /model/result/{video}/adobe\n")
     f.write("chmod -R 0777 /model")
 
 os.system("chmod +x run.sh")
