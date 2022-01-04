36bb99730796844b78f911bb1e64770fdbb7a752
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-04 20:26:07 +0300

    Change interface

--- run.py
+++ run.py
@@ -8,14 +8,13 @@ with open("run.sh", 'w') as f:
     f.write("cd /model/models/modules/DCNv2\n")
     f.write("bash make.sh\n")
     f.write("cd ../../../\n")
-    #f.write("mkdir result\n")
 
     videos = os.listdir("/dataset")
     for video in videos:
-        Path(f"/model/result/{video}").mkdir(parents=True, exist_ok=True)
-        f.write(f"python3 test_single_frame.py /dataset/{video} /model/result/{video}\n")
-        f.write(f"rm -r /model/result/{video}/adobe\n")
-    f.write("chmod -R 0777 /model")
+        f.write(f"mkdir /results/{video}\n")
+        f.write(f"python3 test_single_frame.py /dataset/{video} /results/{video}\n")
+        f.write(f"rm -r /results/{video}/adobe\n")
+    f.write("chmod -R 0777 /results")
 
 os.system("chmod +x run.sh")
 os.system("./run.sh")
